553ccc4c4d51cbf9cd63aa6a53f168df12577918
shaoyi <shaoyimail@163.com>
2020-11-17 23:54:37 +0800

    es环境搭建

---
+++ springboot-elasticsearch/src/main/java/cn/sy/elasticsearch/SpringbootElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package cn.sy.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootElasticsearchApplication.class, args);
+    }
+
+}
---
+++ springboot-elasticsearch/src/test/java/cn/sy/elasticsearch/SpringbootElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sy.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
